61a03d6dec9f1e6e4ae1287f4b6a87ca98130db1
leonardoih <leonardoih090217@gamil.com>
2022-04-02 19:26:38 -0500

    primeros cambios en tarteas

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import freecodecampLogo from './imagenes/freecodecamp-logo.png';
+
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+    <div className='freecodecamp-logo-contenedor'>
+      <img src={freecodecampLogo} alt="" className='freecodecamp-logo' />
+    </div>
+    </div>
+  );
+}
+
+export default App;
